342994f1db7060a6ce4b86c23b474a4937916cbf
Suyeol Jeon <devxoul@gmail.com>
2015-07-23 00:24:30 +0900

    Add test code.

--- NSOperationQueue+CompletionBlockTests/NSOperationQueue_CompletionBlockTests.m
+++ NSOperationQueue+CompletionBlockTests/NSOperationQueue_CompletionBlockTests.m
@@ -1,13 +1,28 @@
+// The MIT License (MIT)
 //
-//  NSOperationQueue_CompletionBlockTests.m
-//  NSOperationQueue+CompletionBlockTests
+// Copyright (c) 2015 Suyeol Jeon (xoul.kr)
 //
-//  Created by 전수열 on 7/23/15.
-//  Copyright (c) 2015 Suyeol Jeon. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import <NSOperationQueue_CompletionBlock/NSOperationQueue+CompletionBlock.h>
 
 @interface NSOperationQueue_CompletionBlockTests : XCTestCase
 
@@ -15,26 +30,14 @@
 
 @implementation NSOperationQueue_CompletionBlockTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testCompletionBlock {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Execute completion block"];
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    queue.completionBlock = ^{
+        [expectation fulfill];
+    };
+    [queue addOperation:[[NSOperation alloc] init]];
+    [self waitForExpectationsWithTimeout:0.5 handler:nil];
 }
 
 @end
