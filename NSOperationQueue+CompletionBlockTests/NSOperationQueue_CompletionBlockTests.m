5d74bf7c35f26985f2f7e2928993801cb661cf50
Suyeol Jeon <devxoul@gmail.com>
2015-07-23 00:50:08 +0900

    Remove import UIKit from test code.

--- NSOperationQueue+CompletionBlockTests/NSOperationQueue_CompletionBlockTests.m
+++ NSOperationQueue+CompletionBlockTests/NSOperationQueue_CompletionBlockTests.m
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 #import <NSOperationQueue_CompletionBlock/NSOperationQueue+CompletionBlock.h>
 
